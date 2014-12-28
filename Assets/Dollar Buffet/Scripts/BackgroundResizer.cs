48e9c4a08ba81c748e62518175ab03bbe8f2b12c
Polydash <polydash.dev@gmail.com>
2014-12-28 16:43:10 +0100

    Added Scene "Dollar Buffet"
    Simple Scene Setup : Camera and Background.
    Added Background Shading : Funny but surely too strong.

---
+++ Assets/Dollar Buffet/Scripts/BackgroundResizer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundResizer : MonoBehaviour
+{
+	private void Start()
+	{
+		Resize();
+	}
+
+	//Call this function whenever the camera aspect ratio
+	//or orthographic size changes
+	public void Resize()
+	{
+		//Compute background scale to fit screen
+		float scaleY = Camera.main.orthographicSize * 2.0f;
+		float scaleX = scaleY * Camera.main.aspect;
+		transform.localScale = new Vector2(scaleX, scaleY);
+	}
+}
