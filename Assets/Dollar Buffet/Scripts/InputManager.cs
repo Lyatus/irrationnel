9168237539675b61c587f383118bc454af73cf06
Polydash <polydash.dev@gmail.com>
2015-01-11 15:59:42 +0100

    Added Negative Cube

---
+++ Assets/Dollar Buffet/Scripts/InputManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+	public Transform m_cubePrefab;
+
+	private void Update()
+	{
+		if(m_cubePrefab && Input.GetKeyDown(KeyCode.Space))
+		{
+			Transform.Instantiate(m_cubePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+		}
+	}
+}
---
+++ Assets/Dollar Buffet/Scripts/MoveCube.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour
+{	
+	Vector3 m_direction;
+
+	void Start()
+	{
+		m_direction = new Vector3(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f));
+	}
+
+	void Update()
+	{
+		transform.eulerAngles = new Vector3(Time.time*120.0f, Time.time*60.0f, 0.0f);
+
+		transform.position += m_direction*Time.deltaTime;
+
+		Vector3 randomDirection = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+		transform.position += randomDirection*Time.deltaTime;
+	}
+}
