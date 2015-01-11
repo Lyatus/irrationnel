1871213bd3a269da6c8efcc3c94efd131198219c
Polydash <polydash.dev@gmail.com>
2015-01-11 17:40:25 +0100

    Added random farts
    10% chance to fart

---
+++ Assets/Dollar Buffet/Scripts/FartAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FartAnimation : MonoBehaviour
+{
+	public float m_animationTime;
+	private float m_elapsed = 0.0f;
+
+	void Update()
+	{
+		m_elapsed += Time.deltaTime;
+		if(m_elapsed >= m_animationTime)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
--- Assets/Dollar Buffet/Scripts/InputManager.cs
+++ Assets/Dollar Buffet/Scripts/InputManager.cs
@@ -4,12 +4,19 @@ using System.Collections;
 public class InputManager : MonoBehaviour
 {
 	public Transform m_cubePrefab;
+	public Transform m_fartPrefab;
+	public float m_fartChance;
 
 	private void Update()
 	{
 		if(m_cubePrefab && Input.GetKeyDown(KeyCode.Space))
 		{
 			Transform.Instantiate(m_cubePrefab, new Vector3(0, 0, 1), Quaternion.identity);
+
+			if(m_fartPrefab && Random.Range(0.0f, 1.0f) < m_fartChance)
+			{
+				Transform.Instantiate(m_fartPrefab, new Vector3(-6, -2, 0), Quaternion.identity);
+			}
 		}
 	}
 }
