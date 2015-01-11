78fd3cb0a35e40aa079c0ea2d65d5a781448bf66
Lyatus (TaxiZord) <lutopia@hotmail.fr>
2015-01-11 17:37:38 +0100

    Improved scene 1 with beautiful women and babies

---
+++ Assets/Side-scroller/Pinups.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pinups : MonoBehaviour {
+	public Sprite[] sprites;
+	SpriteRenderer spriteRenderer;
+	void Start(){
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	void Update () {
+		if(Random.Range(0,200)==0)
+			spriteRenderer.sprite = sprites[Random.Range(0,sprites.Length-1)];
+		else
+			spriteRenderer.sprite = null;
+	}
+}
