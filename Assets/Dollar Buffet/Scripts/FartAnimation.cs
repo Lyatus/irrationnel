19644a3a2667887a70a31fc370e337575a02b8c7
Polydash <polydash.dev@gmail.com>
2015-01-11 18:00:47 +0100

    Added sounds to "Dollar Buffet" scene

--- Assets/Dollar Buffet/Scripts/FartAnimation.cs
+++ Assets/Dollar Buffet/Scripts/FartAnimation.cs
@@ -4,7 +4,14 @@ using System.Collections;
 public class FartAnimation : MonoBehaviour
 {
 	public float m_animationTime;
+	public AudioClip[] m_fartSounds;
 	private float m_elapsed = 0.0f;
+
+	void Start()
+	{
+		int randomSound = Random.Range(0, m_fartSounds.Length);
+		GetComponent<AudioSource>().PlayOneShot(m_fartSounds[randomSound]);
+	}
 
 	void Update()
 	{
--- Assets/Dollar Buffet/Scripts/MrDollarAnimation.cs
+++ Assets/Dollar Buffet/Scripts/MrDollarAnimation.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class MrDollarAnimation : MonoBehaviour
 {
+	public AudioClip[] m_eatingSounds;
+
 	private int m_currentFrame = 1;
 	private Animator m_animator;
 
@@ -15,6 +17,9 @@ public class MrDollarAnimation : MonoBehaviour
 	{
 		if(Input.GetKeyDown(KeyCode.Space))
 		{
+			int randomSound = Random.Range(0, m_eatingSounds.Length);
+			GetComponent<AudioSource>().PlayOneShot(m_eatingSounds[randomSound]);
+
 			m_currentFrame++;
 			if(m_currentFrame > 4) m_currentFrame = 1;
 			m_animator.SetInteger("EatingFrame", m_currentFrame);
